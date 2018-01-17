e2b157d4c1c714ff817292197e34e8ceb47c4d8e
daavisr <dragels@inbox.lv>
2018-01-17 19:25:03 +0200

    Obsolete info deleted from Welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,3 @@
-<!doctype html>
     <style>
         body {
             background: url(img/background.jpg) no-repeat center center fixed!important; 
@@ -18,26 +17,19 @@
         }
     </style>
 @extends('layouts.app')
-
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>FinancialPlanner</title>
-    </head>
-    @section('content')
+@section('content')
     <body>
     <div class="jumbotron">  
         <h1>Financial Planner</h1>
         <h3>Financial Planner is a simple tool created for you to keep your personal finance management much easier.</h3>
         <div class="list">
-        <ul>
-            <li>Make decisions based on given information and improve your finances</li>
-            <li>Update, edit and delete your financial records</li>
-            <li>See detailed information about your finances</li>
-            <li>Add your daily incomes and expenses</li>            
-        </ul>
+            <ul>
+                <li>Make decisions based on given information and improve your finances</li>
+                <li>Update, edit and delete your financial records</li>
+                <li>See detailed information about your finances</li>
+                <li>Add your daily incomes and expenses</li>            
+            </ul>
+        </div>
     </div>
     </body>
     @endsection
