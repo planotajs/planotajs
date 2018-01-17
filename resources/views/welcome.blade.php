d5f664e527804187fcbd0ddf900c205ae0f565eb
RolandsRuja <33718004+RolandsRuja@users.noreply.github.com>
2018-01-17 13:54:20 +0200

    Welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,21 +7,37 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <title>FinancialPlanner</title>
-        
     </head>
+    <style>
+        body {
+            background: url(img/background.jpg) no-repeat center center fixed; 
+            -webkit-background-size: cover;
+            -moz-background-size: cover;
+            -o-background-size: cover;
+            background-size: cover
+        }
+        .jumbotron {
+            margin-top: 150px;
+            text-align: center;
+            color: black;
+            background: rgba(255, 255, 255, 0.9);
+        }
+        li {
+            font-size: 17px;
+        }
+    </style>
     <body>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading"><h4>Home</h4></div>
-
-                    <div class="panel-body">What up</div>
-                </div>
-            </div>
-        </div>
+    <div class="jumbotron">  
+        <h1>Financial Planner</h1>
+        <h3>Financial Planner is a simple tool created for you to keep your personal finance management much easier.</h3>
+        <div class="list">
+        <ul>
+            <li>Make decisions based on given information and improve your finances</li>
+            <li>See your detailed information about your finances</li>
+            <li>Update, edit and delete your financial records</li>
+            <li>Add your daily incomes and expenses</li>            
+        </ul>
     </div>
     </body>
     @endsection
