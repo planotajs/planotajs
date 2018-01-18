c39f8987c07697bbb772a93c68251d109d315232
RolandsRuja <33718004+RolandsRuja@users.noreply.github.com>
2018-01-18 13:16:15 +0200

    Minor design update for Statistics

--- resources/views/statistics.blade.php
+++ resources/views/statistics.blade.php
@@ -2,11 +2,6 @@
 @extends('layouts.app')
 
 @section('content')
-<style>
-    th {
-        text-align: center;
-    }
-</style>
 <div class="container">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
@@ -14,6 +9,7 @@
                     <div class="panel-heading"><h4>Statistics</h4></div>
                     <div class="panel-body">
                         <div class="dateSelector">
+                            <h4>Select time period</h4>
                         {!! Form::label('date', 'Start date', ['class' => 'col-md-4 control-label']) !!}
                                 <div class="col-md-6">
                                 {!! Form::date('date', '', ['class' => 'form-control']) !!}
@@ -23,15 +19,19 @@
                                 {!! Form::date('date', '', ['class' => 'form-control']) !!}
                                 </div>
                         </div>
-                        <div class="panel-body">
-                        {{ Form::submit('Select records', array('class' => 'btn')) }}
+                        <div class="panel-body">                            
+                        {{ Form::submit('Select records', array('class' => 'btn')) }}          
                         <hr>
-                        <h3>Total income: {{$income}}  EUR</h3>
-                        <h3>Total expenses: {{$expenses}} EUR</h3>
-                        <h3><b>Balance: {{$sum}} EUR</b></h3>
+                        <h5>Total income: {{$income}}  EUR</h5>
+                        <h5>Total expenses: {{$expenses}} EUR</h5>
+                        <h5><b>Balance: {{$sum}} EUR</b></h5>
+                        <hr>
+                        <h4 style="text-align: center">Income statistics</h4>
                             <table class="table table-striped">
                                 <tr>
-                                    <th colspan="3">Income</th>
+                                    <th>Category</th>
+                                    <th>Income</th>
+                                    <th>% of total income</th>
                                 </tr>
                                 @foreach ($records as $rec)
                                 <tr>
@@ -41,9 +41,13 @@
                                 </tr>
                                 @endforeach
                             </table>
+                        <hr>
+                        <h4 style="text-align: center">Expenses statistics</h4>
                             <table class="table table-striped">
                                 <tr>
-                                    <th colspan="3">Expenses</th>
+                                    <th>Category</th>
+                                    <th>Expenses</th>
+                                    <th>% of total expenses</th>
                                 </tr>
                                 @foreach ($records as $rec)
                                 <tr>
